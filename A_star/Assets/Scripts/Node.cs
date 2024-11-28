f4e8dd5b51113227844aaf35a10458d95ee5279f
Shuvashis Shrestha <shuvashis.py@gmail.com>
2024-11-28 09:06:22 +0545

    push

---
+++ A_star/Assets/Scripts/Node.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Vector3 position;
+    public List<Node> nodes;
+    public bool isObstacle = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
