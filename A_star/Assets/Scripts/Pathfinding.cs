9bc23633822b300f98cdb8e2c2b2410bf57ee5f2
Shuvashis Shrestha <shuvashis.py@gmail.com>
2024-11-28 22:16:07 +0545

    update

--- A_star/Assets/Scripts/Pathfinding.cs
+++ A_star/Assets/Scripts/Pathfinding.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class Pathfinding : MonoBehaviour
 {
-    
+    NodeGrid grid;
+
+    private void Awake()
+    {
+        grid = GetComponent<NodeGrid>();
+    }
+
+    private void FindPath(Vector3 startPosition, Vector3 endPosition)
+    {
+        Node startNode = grid.GetNodeAtWorldPosition(startPosition);
+        Node targetNode = grid.GetNodeAtWorldPosition(endPosition);
+
+        List<Node> openSet = new List<Node>();
+        HashSet<Node> closedSet = new HashSet<Node>();
+        openSet.Add(startNode);
+    }
 }
